1f01ee7ac76eee378163efe44ae7ee014e9b3a8d
Danilo Carlos Marques <danilo.marques@basis.com.br>
2023-09-21 15:10:06 -0300

    Aulas 08 - Return e Delegação de Generators

--- PHP7/generators.php
+++ PHP7/generators.php
@@ -6,15 +6,32 @@ echo "<hr/>";
 //$range = range(1, 10000);
 
 $range = function() {
-  for ($i = 1; $i <= 10000; $i++) {
+    $sum = 0;
+  for ($i = 1; $i <= 1000; $i++) {
       yield $i;
+      $sum += $i;
   }
+
+  return $sum;
 };
 
+$newRange = function() {
+    yield 1001;
+    yield 1002;
+    yield 1003;
+    yield from [1004, 1005, 1006];
+};
 
-foreach ($range() as $value) {
+$rangeResult = function() use ($range, $newRange) {
+    yield from $range();
+    yield from $newRange();
+};
+
+foreach ($rangeResult() as $value) {
     echo $value . " ";
 }
+
+//var_dump($rangeResult()->getReturn());
 echo "<hr/>";
 
 echo memory_get_usage();
