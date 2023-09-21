1ae9c977e3c3c6e79793d4a93ef9ec4df3fd1d57
Danilo Carlos Marques <danilo.marques@basis.com.br>
2023-09-21 15:43:19 -0300

    Aula 10 - Constantes com valor do tipo Array

---
+++ PHP7/array-de-constantes.php
@@ -0,0 +1,14 @@
+<?php
+
+const DATABASE = [
+    'host' => 'localhost',
+    'user' => 'root',
+    'pass' => 123456,
+    'schema' => 'treinaweb'
+];
+
+
+echo DATABASE['host'] . "<br/>";
+echo DATABASE['user'] . "<br/>";
+echo DATABASE['pass'] . "<br/>";
+echo DATABASE['schema'] . "<br/>";
