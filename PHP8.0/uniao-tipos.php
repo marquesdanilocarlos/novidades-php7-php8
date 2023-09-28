32084cd4539a80d7f28b07805b74ffbe73218860
Danilo Carlos Marques <danilo.marques@basis.com.br>
2023-09-28 14:45:47 -0300

    Aula 24 - União de tipos em retorno, void e false

--- PHP8.0/uniao-tipos.php
+++ PHP8.0/uniao-tipos.php
@@ -1,10 +1,19 @@
 <?php
 
-declare(strict_types=1);
+//declare(strict_types=1);
+
+class Marca
+{}
+
 class Carro
 {
-    public function __construct(public string $marca, public string $cor, public int $ano,)
-    {
+
+    protected int $velocidadeAtual = 10;
+    public function __construct(
+        public string|Marca $marca,
+        public string|int $cor,
+        public int|string $ano,
+    ) {
     }
 
     public function __toString(): string
@@ -12,12 +21,23 @@ class Carro
         return "{$this->marca} - {$this->ano} - {$this->cor}";
     }
 
-    public function andar(int|string $aceleracao, int|string $velocidade,): string|float
+    //Void não é permitido na união de tipos
+    public function andar(int|string $velocidade, int|string|null $aceleracao = null,): string|float
     {
         return "{$aceleracao} - {$velocidade}";
+    }
+
+    public function estaAndando(): int|false /*|bool Não pode bool juntamente com false*/
+    {
+        if ($this->velocidadeAtual > 0) {
+            return $this->velocidadeAtual;
+        }
+
+        return false;
     }
 }
 
 $uno = new Carro(cor: 'preto', marca: "Fiat", ano: 2012);
-echo $uno->andar(velocidade: 110, aceleracao: '5',);
+echo $uno->andar(velocidade: 110,);
 echo $uno;
+echo $uno->estaAndando();
