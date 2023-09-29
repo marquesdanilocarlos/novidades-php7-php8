039f9991b6ca4edab83296e77f8beae3e1a90fbd
Danilo Carlos Marques <danilo.marques@basis.com.br>
2023-09-29 06:14:02 -0300

    Aula 33 - Novas funções string

---
+++ PHP8.0/novas-funcoes-string.php
@@ -0,0 +1,10 @@
+<?php
+
+$contains = str_contains("Treinaweb", "eina");
+var_dump($contains);
+
+$startsWith = str_starts_with("Danilo", "nilo");
+var_dump($startsWith);
+
+$endsWith = str_ends_with("Samara", "ara");
+var_dump($endsWith);
