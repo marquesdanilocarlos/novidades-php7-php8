266572e163e16a6ee7329b28dcfb3010f785609a
Danilo Carlos Marques <danilo.marques@basis.com.br>
2023-09-29 05:56:41 -0300

    Aula 31 - Metodos privados não podem ser final

---
+++ PHP8.0/metodos-privados-final.php
@@ -0,0 +1,17 @@
+<?php
+
+class Veiculo
+{
+    final private function ligar(array $init): float
+    {
+        return 100;
+    }
+}
+
+class Carro
+{
+    public function ligar(array $init): float
+    {
+        return 100;
+    }
+}
