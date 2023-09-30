646ae5e939f25c323a9d0a75c283309cfbad2146
Danilo Carlos Marques <danilo.marques@basis.com.br>
2023-09-30 10:59:52 -0300

    Aula 37 - Intersecção de tipos

---
+++ PHP8.1/intersecao-tipos.php
@@ -0,0 +1,43 @@
+<?php
+
+interface TemNome
+{
+    public function getNome(): string;
+}
+
+interface TemSobreNome
+{
+    public function getSobreNome(): string;
+}
+
+class Pessoa implements TemNome, TemSobreNome
+{
+    public function __construct(
+        public readonly string $nome,
+        public readonly string $sobreNome
+    )
+    {
+    }
+
+    public function getNome(): string
+    {
+        return $this->nome;
+    }
+
+    public function getSobreNome(): string
+    {
+        return $this->sobreNome;
+    }
+
+}
+
+function imprimeNome(TemNome&TemSobreNome $pessoa): void
+{
+    echo "Nome: " . $pessoa->getNome();
+    echo "<br/>";
+    echo "Sobrenome: " . $pessoa->getSobreNome();
+}
+
+$joao = new Pessoa('Jão', 'Lima');
+
+imprimeNome($joao);
